f85bc3ff1ac649ef02394a4449b8c71609e72256
Ritesh Saw <127016413+RITESH-99@users.noreply.github.com>
2024-10-18 09:03:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    window.scrollTo({
+    top: target.offsetTop,
+    behavior: 'smooth'
+    });
+    });
+    });
+    document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Your message has been sent!');
+    });
+    
